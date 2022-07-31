7e57fe493a5dc52fbf23246e5c6079b85df07e38
gongchengxiang <gcx5291@163.com>
2022-07-31 23:13:47 +0800

    webview inited

--- src/App.js
+++ src/App.js
@@ -1,66 +1,132 @@
 import React from 'react';
 import {
-    SafeAreaView,
-    ScrollView,
     StatusBar,
     StyleSheet,
-    Text,
-    useColorScheme,
+    Dimensions,
+    Appearance,
     View,
+    Text,
+    TouchableOpacity,
 } from 'react-native';
+import {WebView} from 'react-native-webview';
 
-import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// 是否暗黑模式，暂时不支持暗黑模式
+const isDarkMode = Appearance.getColorScheme() === 'dark' && false;
+console.log('isDarkMode：', isDarkMode);
+// 状态栏高度
+const statusBarHeight = StatusBar.currentHeight;
+console.log('statusBarHeight', statusBarHeight);
 
-const App = () => {
-    const isDarkMode = useColorScheme() === 'dark';
-
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+// 主页面渲染
+class App extends React.Component {
+    webview = React.createRef();
+    state = {
+        appInited: false,
+    };
+    onWebviewLoadEnd = data => {
+        console.log('loaded');
+        const jsCode = `
+            const rnMsg = 'hello, webview!'
+            // alert(rnMsg) 
+        `;
+        this.webview.current.injectJavaScript(jsCode);
+        // setTimeout(() => {
+        //     this.handleAppInit();
+        // }, 5000);
+    };
+    handleAppInit = () => {
+        console.log(1);
+        this.setState(() => ({
+            appInited: true,
+        }));
     };
 
-    return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-            />
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={backgroundStyle}>
-                <View
-                    style={{
-                        backgroundColor: isDarkMode
-                            ? Colors.black
-                            : Colors.white,
-                    }}>
-                    <Text>hello, freefire!</Text>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
-};
+    render() {
+        const {webappUrl, appInited} = this.state;
+        return (
+            <View style={styles.appWrap}>
+                <StatusBar
+                    barStyle="dark-content"
+                    translucent={true}
+                    backgroundColor="transparent"
+                />
 
+                {/* 不显示webview的时候显示view，作为缓冲区 */}
+                {!appInited && (
+                    <View style={styles.indexView}>
+                        <TouchableOpacity onPress={this.handleAppInit}>
+                            <Text>缓冲区，用于初始化显示内容</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+
+                {/* webview第一时间渲染，缩短渲染时间 */}
+                <WebView
+                    style={styles.webviewStyle}
+                    ref={this.webview}
+                    source={{
+                        uri: 'https://baidu.com',
+                    }}
+                    onLoadEnd={this.onWebviewLoadEnd}
+                    // originWhitelist={['https://*', 'http://*']}
+                    // originWhitelist={['*', 'file://', 'https://*', 'http://*']}
+                    // javaScriptEnabled={true}
+                    // scalesPageToFit={false}
+                    // mediaPlaybackRequiresUserAction={false}
+                    // javaScriptCanOpenWindowsAutomatically={true}
+                    // scrollEnabled={true}
+                    // bounces={false}
+                    // setBuiltInZoomControls={false}
+                    // mixedContentMode="always"
+                    // allowingReadAccessToURL={'file://'}
+                    // allowUniversalAccessFromFileURLs={true}
+                    // allowFileAccessFromFileURLS={true}
+                    // allowFileAccess={true}
+                    // cacheEnabled={true}
+                    // cacheMode={'LOAD_DEFAULT'}
+                    // allowsFullscreenVideo={true}
+                    // allowsInlineMediaPlayback={true}
+                    // domStorageEnabled={true}
+                    // thirdPartyCookiesEnabled={true}
+                    // textZoom={100}
+                    // setSupportMultipleWindows={true}
+                    // onShouldStartLoadWithRequest={
+                    //     this.onShouldStartLoadWithRequest
+                    // }
+                    // onNavigationStateChange={this.onNavigationStateChange}
+                />
+            </View>
+        );
+    }
+}
+
+const {height, width} = Dimensions.get('window');
 const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
+    appWrap: {
+        width,
+        height,
+        position: 'relative',
+        zIndex: 0,
     },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
+    indexView: {
+        width,
+        height,
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: 2,
     },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
+    webviewStyle: {
+        width,
+        height,
+        backgroundColor: 'white',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: 1,
     },
 });
 
